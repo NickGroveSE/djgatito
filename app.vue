57e05896096396a61eb6d29610fe4e586f5ff2c4
Nick Grove <nickgrove.se@gmail.com>
2024-02-29 21:06:43 -0500

    First Commit: Metallic Background and Album Art

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+  div {
+    width: 500px; 
+    height: 500px;
+    margin: calc(50vh - 250px) auto 0 auto;
+    border-radius: 5px; 
+    opacity: 1; 
+    background-image: url("https://i1.sndcdn.com/artworks-aWYsfzNUWCCNZTNw-zPe9lQ-t500x500.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+</style>
